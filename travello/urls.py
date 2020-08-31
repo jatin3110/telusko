e81dcc97c2ef81017bfd8b09c5bc61ccc0a509c3
JATIN GROVER <jatingrover3110@gmail.com>
2020-09-01 00:41:46 +0530

    Making data dynamic/ using if else statements

--- travello/urls.py
+++ travello/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('telusko',views.index,name='index')
+    path("",views.index, name="index")
 ]
--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
-
+from . models import Destination
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    
+    dest1 = Destination()
+    dest1.name = 'Mumbai'
+    dest1.desc = 'The city that never sleeps'
+    dest1.price = 700
+    dest1.image = 'destination_1.jpg'
+    dest1.offer = False
+
+    dest2 = Destination()
+    dest2.name = 'Hyderabad'
+    dest2.desc = 'Biryani'
+    dest2.price = 680
+    dest2.image = 'destination_2.jpg'
+    dest2.offer = True
+
+    dest3 = Destination()
+    dest3.name = 'Banglore'
+    dest3.desc = 'IT hub'
+    dest3.price = 500
+    dest3.image = 'destination_3.jpg'
+    dest3.offer = False
+
+    dests = [dest1, dest2, dest3]
+    
+    return render(request,"index.html", {'dests': dests})
