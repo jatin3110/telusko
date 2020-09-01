91c5064caae42656735a99c10731e0e554be49bf
JATIN GROVER <jatingrover3110@gmail.com>
2020-09-02 01:01:06 +0530

    user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User,auth
 # Create your views here.
 
 
 def register(request)
+if request.method == 'POST'
+
+    first_name = request.POST['first_name']
+
+
+    user= User.objects.create_user(username=username,password=password1, email=email)
+else:
     return render(request,'register.html')
