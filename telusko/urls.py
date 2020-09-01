21f35d54843dcf5bdcceab2abfdbe8c7bf16ecf8
JATIN GROVER <jatingrover3110@gmail.com>
2020-09-01 23:51:30 +0530

    connecting databaes

--- telusko/urls.py
+++ telusko/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('travello', include('travello.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.url'))
 ]
 
-# hello
-
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
